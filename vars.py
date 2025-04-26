df0d5e5aba53e7471bf030cfe79015188d5aa112
rnpublic88628867 <rnpublicschool7761@gmail.com>
2025-04-26 09:40:49 +0530

    Delete vars.py

--- vars.py
+++
@@ -1,8 +0,0 @@
-# Don't Remove Credit Tg - @Tushar0125
-# Ask Doubt on telegram @Tushar0125
-
-from os import environ
-
-API_ID = int(environ.get("API_ID", "29685534"))
-API_HASH = environ.get("API_HASH", "5204f93bb78feb40bedf61cad13df543")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
